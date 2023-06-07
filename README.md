1ebf437ed7c665599c4e5280acd20a23f232603f
Marcel Brode <m.brode@shopware.com>
2023-06-07 13:29:13 +0200

    INITIAL COMMIT

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # sw6-license-updater
+
 Simple bash script to quickly upgrade your systemConfig variables for SwagCommercial
+
+## How to config?
+
+At first, open the `.config` file and configure the three variables necessary:
+
+- `SHOPWARE_PATH`: The path to your shopware instance
+- `LICENSE_HOST`: The license host to be added into your SW6 database. Mostly `localhost` suffices here
+- `LICENSE_TOGGLES`: A JSON blob to know which feature toggles to register within the SBP
+
+## How to use?
+
+After setting up the variables mentioned above this command will do the trick:
+
+```bash
+bash .license.sh
+```
+
+The only prompt you'll be receiving will asking for the `Shopware-X` token, you get via the SBP. After that, you're already done.
---
+++ license.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+. ./.config;
+
+if [[ -z $SHOPWARE_PATH ]]; then
+  echo "Your SHOPWARE_PATH variable has not been set";
+  exit;
+elif [[ -z $LICENSE_HOST ]]; then
+  echo "Your LICENSE_HOST variable has not been set";
+  exit;
+elif [[ -z $LICENSE_TOGGLES ]]; then
+  echo "Your LICENSE_TOGGLES variable has not been set";
+  exit;
+fi
+
+echo 'Please enter your Shopware-X token:';
+read shopware_x;
+
+license_key=$(curl --silent --request POST \
+  --url https://api.shopware.com/internal/commerciallicensekeysforshopwaredevelopers \
+  --header 'Content-Type: application/json' \
+  --header "X-Shopware-Token: $shopware_x" \
+  --data "{
+    \"licenseHost\": \"$LICENSE_HOST\",
+    \"licenseToggles\": $LICENSE_TOGGLES
+}" | jq -r '.key');
+
+if [[ "$license_key" == "null" ]]; then
+  printf "\n";
+  echo "Entered Shopware-X token was invalid. Could not set license information.";
+else
+  $SHOPWARE_PATH/bin/console system:config:set core.store.licenseHost $LICENSE_HOST;
+  $SHOPWARE_PATH/bin/console system:config:set core.store.licenseKey $license_key;
+fi
